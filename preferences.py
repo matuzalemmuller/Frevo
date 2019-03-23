068ab5cb7c82829ff126fafd0c6d1fa3a875e5aa
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-03-23 18:02:42 -0300

    Limit ui sizes and add event to handle bttn click

--- preferences.py
+++ preferences.py
@@ -6,11 +6,24 @@ class Preferences(QDialog):
 
     def __init__(self):
         super().__init__()
+        self.width = 450
+        self.height = 80
+        self.setFixedSize(self.width, self.height)
+        self.setWindowTitle("Preferences")
+        
         # Textbox
         textbox = QLineEdit(self)
         textbox.move(20, 20)
-        textbox.resize(400,20)
+        textbox.resize(self.width - 50,20)
+        
         # Save button
         button = QPushButton("Save", self)
-        button.move(200,50)
+        button.setFixedWidth(80)
+        button.move((self.width/2)-(button.width()/2),50)
+        button.clicked.connect(self._save_command)
+        
         self.exec_()
+
+
+    def _save_command(self):
+        print("File saved!")
