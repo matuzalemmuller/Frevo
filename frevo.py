817c92638a768b3369e20db9b7db8aea5e41287c
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-03-23 12:35:55 -0300

    Add dark mode detection to choose icon color

--- frevo.py
+++ frevo.py
@@ -1,12 +1,22 @@
 import AppKit
 import sys
 from tray_icon import TrayIcon
+from Foundation import NSUserDefaults
 
 # To hide app from Mac dock
 # info = AppKit.NSBundle.mainBundle().infoDictionary()
 # info["LSBackgroundOnly"] = "1"
 
-app = TrayIcon()
+# Detects if OSX is in dark mode to choose icon color
+mode = NSUserDefaults.standardUserDefaults().stringForKey_('AppleInterfaceStyle')
+
+if mode == "Dark":
+    icon = "logo-white.png"
+else:
+    icon = "logo-black.png"
+
+# Creates tray icon
+app = TrayIcon(icon)
 app.exec_()
 app.setQuitOnLastWindowClosed(False)
 
