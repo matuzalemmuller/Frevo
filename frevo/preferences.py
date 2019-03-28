b207ce8cdb415fa5d13667127a777c3352c3a03c
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-03-28 00:14:08 -0300

    Create layout for commands in separate method

--- frevo/preferences.py
+++ frevo/preferences.py
@@ -11,68 +11,12 @@ class Preferences(QDialog):
     def __init__(self, tray):
         super().__init__()
         self._tray = tray
-        self._width = 720
-        self._height = 80
-        self.setFixedSize(self._width, self._height)
         self.setWindowTitle("Preferences")
         validator=QtCore.QRegExp("[A-Za-z0-9- _]+")
         self._nameValidator = QtGui.QRegExpValidator(validator)
-        name, terminal, command = self._read_command()
 
-        # Name textbox
-        self._nameTextbox = QLineEdit(self)
-        self._nameTextbox.move(20, 20)
-        self._nameTextbox.resize(80,20)
-        self._nameTextbox.setText(name)
-        self._nameTextbox.setMaxLength(15)
-        self._nameTextbox.setValidator(self._nameValidator)
-
-        # Command textbox
-        self._commandTextbox = QLineEdit(self)
-        self._commandTextbox.move(120, 20)
-        self._commandTextbox.resize(self._width - 415,20)
-        self._commandTextbox.setText(command)
-        
-        # Clear button
-        self._clearButton = QPushButton("Clear", self)
-        self._clearButton.setFixedWidth(70)
-        self._clearButton.move(self._width - 280,15)
-        self._clearButton.clicked.connect(self._clear_text) 
-
-        # Run button
-        self._runButton = QPushButton("Run", self)
-        self._runButton.setFixedWidth(60)
-        self._runButton.move(self._width - 220,15)
-        self._runButton.clicked.connect(self._run)
-
-        # Checkbox to run in terminal
-        self._terminalCheckbox = QCheckBox("Launch in Terminal", self)
-        if terminal == None:
-            self._terminalCheckbox.setChecked(True)
-        else:
-            self._terminalCheckbox.setChecked(terminal)
-        self._terminalCheckbox.move(self._width-150,20)
-       
-        # Save button
-        self._saveButton = QPushButton("Save", self)
-        self._saveButton.setFixedWidth(80)
-        self._saveButton.move(self._width/2,50)
-        self._saveButton.clicked.connect(self._save_command)
-
-        # Cancel button
-        self._cancelButton = QPushButton("Cancel", self)
-        self._cancelButton.setFixedWidth(80)
-        self._cancelButton.move((self._width/2)-self._cancelButton.width(),50)
-        self._cancelButton.clicked.connect(self._cancel_command)
-        self._cancelButton.setDefault(True)
-
-        # Name & Command text above fields
-        self._commandDesc = QLabel(self)
-        self._commandDesc.setText("Name")
-        self._commandDesc.move(20,3)
-        self._nameDesc = QLabel(self)
-        self._nameDesc.setText("Command")
-        self._nameDesc.move(120,3)
+        self._windowLayout = self._createLayout()        
+        self.setLayout(self._windowLayout)
 
         self.activateWindow()
         self.exec_()
@@ -119,3 +63,84 @@ class Preferences(QDialog):
         self._tray.run_command(self._commandTextbox.text(),
                                self._terminalCheckbox.isChecked())
         self.activateWindow()
+
+
+    def _createLayout(self):
+        name_list, terminal_list, command_list = ConfigHandler().read_commands()
+
+        layout = []
+
+        for i in range(len(command_list)):
+            # Name & Command text above fields
+            nameDesc = QLabel(self)
+            nameDesc.setText("Name")
+            commandDesc = QLabel(self)
+            commandDesc.setText("Command")
+
+            # Name textbox
+            nameTextbox = QLineEdit(self)
+            nameTextbox.resize(80,20)
+            nameTextbox.setMaxLength(15)
+            nameTextbox.setMaximumWidth(80)
+            nameTextbox.setValidator(self._nameValidator)
+            nameTextbox.setText(name_list[i])
+
+            # Command textbox
+            commandTextbox = QLineEdit(self)
+            commandTextbox.resize(320,20)
+            commandTextbox.setText(command_list[i])
+            
+            # Clear button
+            clearButton = QPushButton("Clear", self)
+            clearButton.setFixedWidth(70)
+            clearButton.clicked.connect(self._clear_text)
+
+            # Run button
+            runButton = QPushButton("Run", self)
+            runButton.setFixedWidth(60)
+            runButton.clicked.connect(self._run)
+
+            # Checkbox to run in terminal
+            terminalCheckbox = QCheckBox("Launch in Terminal", self)
+            if terminal_list[i] == None:
+                terminalCheckbox.setChecked(True)
+            else:
+                terminalCheckbox.setChecked(terminal_list[i])
+
+            layout.append(QGridLayout())
+            layout[i].addWidget(nameDesc, 1, 1)
+            layout[i].addWidget(commandDesc, 1, 2)
+            layout[i].addWidget(nameTextbox, 2, 1)
+            layout[i].addWidget(commandTextbox, 2, 2)
+            layout[i].addWidget(clearButton, 2, 3)
+            layout[i].addWidget(runButton, 2, 4)
+            layout[i].addWidget(terminalCheckbox, 2, 5)
+            layout[i].setContentsMargins(10,5,10,5)
+            layout[i].setHorizontalSpacing(10)
+            layout[i].setVerticalSpacing(0)
+            layout[i].setColumnMinimumWidth(2,300)
+
+
+        cancelButton = QPushButton("Cancel", self)
+        cancelButton.setFixedWidth(80)
+        cancelButton.clicked.connect(self._cancel_command)
+        cancelButton.setDefault(True)
+
+        # Save button
+        saveButton = QPushButton("Save", self)
+        saveButton.setFixedWidth(80)
+
+        windowLayout = QGridLayout()
+        windowLayout.setVerticalSpacing(0)
+
+        for i in range(len((layout))):
+            windowLayout.addLayout(layout[i],i,1)
+
+        closeLayout = QGridLayout()
+        closeLayout.addWidget(cancelButton, 1, 1)
+        closeLayout.addWidget(saveButton, 1, 2)
+
+        windowLayout.addLayout(closeLayout,windowLayout.rowCount()+1,1, QtCore.Qt.AlignCenter)
+        windowLayout.setContentsMargins(10,5,5,5)
+
+        return windowLayout
